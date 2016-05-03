37ed12eacbf4f65db108c1bb2ec437a92c233b30
Bogdan Pshonyak <bpshonyak@live.com>
2016-05-03 14:23:58 -0700

    Created skeleton for TimesheetTester class

--- src/TimesheetTester.java
+++ src/TimesheetTester.java
@@ -3,4 +3,59 @@
  */
 public class TimesheetTester {
 
+    private Timesheet timesheet;
+
+    public TimesheetTester() {
+        timesheet = new Timesheet();
+    }
+
+    // General Tests -------------------------------------------------
+
+    public boolean testEmptyTimesheet(){
+        double pay = timesheet.calculatePay();
+        return pay == 0.0;
+    }
+
+    //168 = 24*7
+    public boolean testMaxHours(){
+        return true;
+    }
+
+    public boolean testMaxHoursPerDay(){
+        return true;
+    }
+
+    public boolean testNegativeHoursPerDay(){
+        return true;
+    }
+
+
+    // Function tests ------------------------------------------------
+
+    //Over or under 7 days
+    public boolean testSetWeekHours(){
+        return true;
+    }
+
+    public boolean testSetDayHours(){
+        return true;
+    }
+
+    public boolean testCalculatePay(){
+        return true;
+    }
+
+    // Specification tests --------------------------------------------
+
+    public boolean testFristInput(){
+        return true;
+    }
+
+    public boolean testSecondInput(){
+        return true;
+    }
+
+    public boolean testThirdInput(){
+        return true;
+    }
 }
